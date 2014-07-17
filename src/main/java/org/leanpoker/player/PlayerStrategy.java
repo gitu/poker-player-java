2237e2990f1192b3001938d6c45ba7f683391635
Florian Besser <flbe@zuehlke.com>
2014-07-17 17:46:54 +0200

    Changed Sgtrategy to always calling

--- src/main/java/org/leanpoker/player/PlayerStrategy.java
+++ src/main/java/org/leanpoker/player/PlayerStrategy.java
@@ -2,13 +2,13 @@ package org.leanpoker.player;
 
 public class PlayerStrategy {
 	
-    static final String VERSION = "Default Java folding player with logging";
+    static final String VERSION = "Always calling player";
 
     public static int betRequest(GameState gameState) {
     	
     	System.out.println("Parsed Gamestate: " + gameState.toString());
     	
-        return 0;
+        return gameState.getCurrent_buy_in() - gameState.getPlayers()[gameState.getIn_action()].getBet();
     }
 
     public static void showdown(GameState gameState) {
