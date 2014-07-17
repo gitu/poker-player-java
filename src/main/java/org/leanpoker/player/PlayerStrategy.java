53c25bbc13f85f35b750c50c04b18987c9a7cc37
Florian Besser <flbe@zuehlke.com>
2014-07-17 19:09:55 +0200

    implemented preFlopStrategy

--- src/main/java/org/leanpoker/player/PlayerStrategy.java
+++ src/main/java/org/leanpoker/player/PlayerStrategy.java
@@ -6,18 +6,88 @@ public class PlayerStrategy {
 
     public static int betRequest(GameState gameState) {
     	
-    	System.out.println("Parsed Gamestate: " + gameState.toString());
+    	//System.out.println("Parsed Gamestate: " + gameState.toString());
     	
         int bet = gameState.getCurrent_buy_in() - gameState.getPlayers()[gameState.getIn_action()].getBet();
-        
+    	
+        if (gameState.getCommunity_cards().length == 0) {
+    		bet = preFlopStrategy(gameState);
+    	} else {
+    		
+    	}
+
         System.out.println("Betting: " + bet);
         
 		return bet;
     }
 
-    public static void showdown(GameState gameState) {
+    private static int preFlopStrategy(GameState gameState) {
+    	int strength = computeHoleCardStrength(gameState.getPlayers()[gameState.getIn_action()]);
+    	System.out.println("Determined we are currently in the preFlop. Card Strength is: " + strength);
+    	if (strength <= 27) {
+    		return 0;
+    	} else {
+			int call = gameState.getCurrent_buy_in() - gameState.getPlayers()[gameState.getIn_action()].getBet();
+			if (strength <= 31) {
+				//Call
+				return call;
+			} else {
+				//Max of (Call, Raise)
+				return Math.max(call, gameState.getSmall_blind() * 10);
+			}
+		}
+	}
+
+	public static void showdown(GameState gameState) {
 
     	System.out.println("Parsed Gamestate: " + gameState.toString());
 
     }
+    
+	private static int computeHoleCardStrength(Player player) {
+		Card[] hole_cards = player.getHole_cards();
+		Card first = hole_cards[0];
+		Card second = hole_cards[1];
+		int result = 0;
+		
+		result += getSingleCardStrength(first);
+		result += getSingleCardStrength(second);
+		
+		if (first.getRank() == second.getRank()) {
+			result += 10;
+		}
+		if (first.getSuit() == second.getSuit()) {
+			result += 4;
+		}
+		if (Math.abs(first.getRank().getValue() - second.getRank().getValue()) == 1) {
+			result += 3;
+		}
+		if (Math.abs(first.getRank().getValue() - second.getRank().getValue()) == 2) {
+			result += 2;
+		}
+		if (Math.abs(first.getRank().getValue() - second.getRank().getValue()) == 3) {
+			result += 1;
+		}
+		
+		//TODO: Also include position at table into this: http://www.poker1x1.de/poker1x1/starthands_poker.php
+		
+		return result;
+	}
+
+	private static int getSingleCardStrength(Card card) {
+		switch(card.getRank()) {
+		case ACE:
+			return 16;
+		case KING:
+			return 14;
+		case QUEEN:
+			return 13;
+		case JACK:
+			return 12;
+		case TEN:
+			return 11;
+		default:
+			return card.getRank().getValue();
+		}
+	}
 }
--- src/main/java/org/leanpoker/player/Rank.java
+++ src/main/java/org/leanpoker/player/Rank.java
@@ -3,7 +3,17 @@ package org.leanpoker.player;
 import org.codehaus.jackson.annotate.JsonCreator;
 
 public enum Rank {
-	ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
+	ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(14);
+	
+	private int value;
+	
+	Rank(int v) {
+		value = v;
+	}
+	
+	public int getValue() {
+		return value;
+	}
 	
 	@JsonCreator
 	public static Rank forValue(String v) {
